52c01efc793de2173c70ed4c95a9cd9c8acf7274
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2024-07-11 23:58:24 -0400

    Setting up knex schema

--- backend/backend/migrations/20240711230837_01-users.js
+++ backend/backend/migrations/20240711230837_01-users.js
@@ -3,7 +3,42 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+
+  return knex.schema
+
+		.createTable('program', (table) => {
+			table.increments();
+			table.text('program_name', 128).unique().notNullable();
+		})
+
+		.createTable('role', (table) => {
+			table.increments();
+			table.text('role_name', 128).unique().notNullable();
+		})
+
+		.createTable('users', (table) => {
+			table.increments('id').primary();
+			table.string('first_name').notNullable();
+			table.string('last_name').notNullable();
+			table
+				.integer('program')
+				.notNullable()
+				.unsigned()
+				.references('id')
+				.inTable('program')
+				.onUpdate('CASCADE')
+				.onDelete('CASCADE');
+			table.string('sprint').notNullable();
+			table.integer('points').notNullable();
+			table.integer('role')
+				.notNullable()
+				.unsigned()
+				.references('id')
+				.inTable('role')
+				.onUpdate('CASCADE')
+				.onDelete('CASCADE');
+		});
+
 };
 
 /**
@@ -11,5 +46,8 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema
+		.dropTableIfExists('users')
+		.dropTableIfExists('role')
+		.dropTableIfExists('program');
 };
